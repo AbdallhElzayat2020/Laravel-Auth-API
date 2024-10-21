2a88142434789b4900bee1c34ed3b98d9fbe4472
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-10-22 00:39:26 +0300

    Data Fetched Success

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,6 +11,9 @@ class PostController extends Controller
     public function index()
     {
         $posts = Posts::all();
-        return response()->json($posts);
+        // return response($posts, 200);
+        $msg = ['Data fetched successfully'];
+
+        return response()->json($posts, 200, $msg);
     }
 }
