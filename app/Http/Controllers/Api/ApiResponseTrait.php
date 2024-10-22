b670148dc784209d7a4984daaf6317f33497e0fb
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-10-22 10:54:39 +0300

    api PostResource

--- app/Http/Controllers/Api/ApiResponseTrait.php
+++ app/Http/Controllers/Api/ApiResponseTrait.php
@@ -12,8 +12,6 @@ trait ApiResponseTrait
     public function apiResponse($data = null, $message = null, $status = null)
     {
 
-
-
         return response()->json([
             'data' => $data,
             'message' => $message,
--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Posts;
 
 class PostController extends Controller
@@ -11,7 +12,7 @@ class PostController extends Controller
     use ApiResponseTrait;
     public function index()
     {
-        $posts = Posts::all();
+        // $posts = Posts::all();
 
         // $array = [
         //     'data' => $posts,
@@ -20,12 +21,16 @@ class PostController extends Controller
         // ];
 
         // return response()->json($array);
+        $posts = Posts::all();
+
+        $posts = PostResource::collection($posts);
 
         return $this->apiResponse($posts, 'success', 200);
     }
     public function show($id)
     {
-        $post = Posts::find($id);
+        $post = new PostResource(Posts::find($id));
+
         if ($post) {
 
             return $this->apiResponse($post, 'success', 200);
